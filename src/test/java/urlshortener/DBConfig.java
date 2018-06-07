8187b64b30dfd9a2068897cab2c27f04657e4bf2
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-06-08 01:44:42 +0800

    connect to elephant sql

---
+++ src/test/java/urlshortener/DBConfig.java
@@ -0,0 +1,22 @@
+package urlshortener;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DBConfig{
+
+//       @Bean
+//       public DataSource dataSource(){
+//          DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//          dataSource.setDriverClassName("org.postgresql.Driver");
+//          dataSource.setUrl("jdbc:postgresql://elmer.db.elephantsql.com/gehaqosv");
+//          dataSource.setUsername( "gehaqosv" );
+//          dataSource.setPassword( "Nb1M7we6tZTIRP9LK4SNnnnmtUzEZYZ9" );
+//          return dataSource;
+//       }
+
+}
